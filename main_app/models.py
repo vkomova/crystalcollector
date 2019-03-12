6fc5edd7148176956a6a6d2ac67f56faa6d9fac8
Valerie Komova <valeriyakomova@gmail.com>
2019-03-12 08:47:17 -0700

    updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Crystal(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+    healingproperties = models.TextField()
+
+    def __str__(self):
+        return self.name
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('crystals/', views.crystals_index, name='index'),
+    path('crystals/<int:crystal_id>/', views.crystals_detail, name='detail'),
+    
 ]
