fbfc05c863797215f48a3275234596fc93d89ff5
Valerie Komova <valeriyakomova@gmail.com>
2019-03-13 15:27:26 -0700

    one more update

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('crystals/<int:pk>/delete/', views.CrystalDelete.as_view(), name='crystals_delete'),
     path('crystals/<int:crystal_id>/add_uses/', views.add_uses, name='add_uses'),
     path('crystals/<int:crystal_id>/assoc_country/<int:country_id>/', views.assoc_country, name='assoc_country'),
+    path('crystals/<int:crystal_id>/unassoc_country/<int:country_id>/', views.unassoc_country, name='unassoc_country'),
     path('countries/', views.CountryList.as_view(), name='countries_index'),
     path('countries/<int:pk>/', views.CountryDetail.as_view(), name='countries_detail'),
     path('countries/create/', views.CountryCreate.as_view(), name='countries_create'),
